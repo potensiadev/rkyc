a450fa26bc68262929ee75b72895376b212cba3f
prohustler <prohustler100@gmail.com>
2026-01-22 00:42:55 +0900

    ljfldfdf

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import Index from "./pages/Index";
 import CorporationSearch from "./pages/CorporationSearch";
 import DirectSignalPage from "./pages/DirectSignalPage";
@@ -29,8 +28,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard" element={<Index />} />
+          <Route path="/" element={<Index />} />
           <Route path="/corporations" element={<CorporationSearch />} />
           <Route path="/briefing" element={<DailyBriefingPage />} />
           <Route path="/analytics" element={<AnalyticsStatusPage />} />
